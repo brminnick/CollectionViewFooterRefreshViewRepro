47cd2b1b7eb9ef2e7f2dc1f6d7cb944de1e977e2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-03-30 12:41:27 -0700

    First Commit

---
+++ CollectionViewFooterRefreshViewRepro/App.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms;
+
+namespace CollectionViewFooterRefreshViewRepro
+{
+    public class App : Application
+    {
+        public App() => MainPage = new NavigationPage(new SelectionPage());
+    }
+
+    class SelectionPage : ContentPage
+    {
+        public SelectionPage()
+        {
+            var withFooterButton = new Button
+            {
+                Text = "CollectionView with Footer"
+            };
+            withFooterButton.Clicked += async (sender, e) => await Navigation.PushAsync(new CollectionViewPage(true));
+
+            var noFooterButton = new Button
+            {
+                Text = "CollectionView without Footer"
+            };
+            noFooterButton.Clicked += async (sender, e) => await Navigation.PushAsync(new CollectionViewPage(false));
+
+            Content = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Children =
+                {
+                    withFooterButton,
+                    noFooterButton
+                }
+            };
+        }
+    }
+}
---
+++ CollectionViewFooterRefreshViewRepro/CollectionViewPage.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewFooterRefreshViewRepro
+{
+    class CollectionViewPage : ContentPage
+    {
+        readonly CollectionView _collectionView;
+        readonly RefreshView _refreshView;
+
+        public CollectionViewPage(bool shouldUseFooter)
+        {
+            Title = shouldUseFooter ? "Refresh Indicator is Hidden" : "Refresh Indicator is Visible";
+
+            _collectionView = new CollectionView
+            {
+                Footer = shouldUseFooter ? new BoxView { HeightRequest = 20 } : null
+            };
+
+            _refreshView = new RefreshView
+            {
+                RefreshColor = Color.Black,
+                Content = _collectionView,
+                Command = new Command(async () => await GenerateItemSource())
+            };
+
+            Content = _refreshView;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            _refreshView.IsRefreshing = true;
+        }
+
+        async Task GenerateItemSource()
+        {
+            var itemSource = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+                itemSource.Add($"Row {i}");
+
+            _collectionView.ItemsSource = itemSource;
+
+            await Task.Delay(2000);
+
+            _refreshView.IsRefreshing = false;
+        }
+    }
+}
